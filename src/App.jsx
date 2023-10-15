1d9ba8c6f0e20524ba9f10a1651bfd1a33c18a1b
Baleeeee <2010631170012@student.unsika.ac.id>
2023-10-15 22:32:14 +0700

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import {Routes, Route} from 'react-router-dom';
+
+import NavbarComponent from './components/NavbarComponent';
+import FooterComponent from './components/FooterComponent';
+
+
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ExperiencePage from './pages/ExperiencePage';
+import SkillPage from './pages/SKillPage';
+import InterestPage from './pages/InterestPage';
+import AwardsPage from './pages/AwardsPage';
+
+
+
+function App() {
+  return (
+  <div>
+    <NavbarComponent />
+
+    <Routes>
+      <Route path='/' Component={HomePage} />
+      <Route path='/about' Component={AboutPage} />
+      <Route path='/experience' Component={ExperiencePage} />
+      <Route path='/skill' Component={SkillPage} />
+      <Route path='/interest' Component={InterestPage} />
+      <Route path='/Awards' Component={AwardsPage} />
+    </Routes>
+
+    <FooterComponent />
+  </div>
+  );
+}
+
+export default App;
